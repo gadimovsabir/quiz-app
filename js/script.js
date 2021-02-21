f28ab625fe5655d5259c8fab2fc934f68169051f
Sabir <gadimovsabir@gmail.com>
2021-02-21 11:44:55 +0400

    Add finishExam and showResult functions

--- js/script.js
+++ js/script.js
@@ -1,106 +1,133 @@
 "use strict";
 
-// Bu formada göndər json-u
-// getQuestions funkçiyasinda sorgu gedir
-// Cavab massiv olsun 15 obyektdən ibarət
+// JSON bu formatda olmalidi
 // [
 //     {
 //         "question": "1-ci sual",
-//         "answers": [
-//             "1-ci variant",
-//             "2-ci variant",
-//             "3-cü variant",
-//             "4-cü variant"
-//         ],
-//         "selected": null
+//         "answers": ["1", "2", "3", "4"]
 //     },
 //     ...
 // ]
 
 const startExamBtn = document.querySelector("#start-exam");
 const quizContainer = document.querySelector(".quiz-container");
+const questionNumbers = document.querySelectorAll(".quiz-timeline span");
 const questionContainer = document.querySelector(".quiz-question p");
 const variantsList = document.querySelector(".quiz-question ul");
-variantsList.addEventListener("change", chooseOption);
+const resultContainer = document.querySelector(".result-container");
+const resultFields = document.querySelectorAll(".result-field");
 
+let currentQuestionIndex = +sessionStorage.currentQuestion || 0;
 let questionsArray;
-let currentQuestionIndex = 0;
-let questionNumbers = document.querySelectorAll(".quiz-timeline > span");
+
+startExamBtn.addEventListener("click", getQuestions);
+variantsList.addEventListener("change", chooseOption);
+document.querySelector("#finish-exam").addEventListener("click", finishExam);
 
 questionNumbers.forEach((item) => {
     item.addEventListener("click", (event) => {
-        setQuestion(event.target.textContent - 1);
+        setQuestion(event.target.textContent - 1, currentQuestionIndex);
         event.stopPropagation();
     });
 });
 
-document.querySelector("#finish-exam").addEventListener("click", finishExam);
-
 if (!sessionStorage.questions) {
     quizContainer.style.display = "none";
-    startExamBtn.addEventListener("click", getQuestions);
 } else {
     startExamBtn.parentElement.style.display = "none";
     questionsArray = JSON.parse(sessionStorage.getItem("questions"));
-    setQuestion(0);
+    setQuestion(currentQuestionIndex, currentQuestionIndex);
 }
 
 async function getQuestions(event) {
     let response = await fetch("");
     let json = await response.json();
-	questionsArray = JSON.parse(json);
-	questionsArray.forEach((item) => setRandomVariants(item.answers));
-	sessionStorage.setItem("questions", JSON.stringify(questionsArray));
+    questionsArray = JSON.parse(json);
+    questionsArray.forEach((item) => {
+        item.ra = setRandomVariants(item.answers);
+        item.selected = null;
+    });
+
+    sessionStorage.setItem("questions", JSON.stringify(questionsArray));
     startExamBtn.parentElement.style.display = "none";
+    resultContainer.classList.remove("d-flex", "center");
     quizContainer.style.display = "block";
-    setQuestion(0);
+    setQuestion(0, 0);
     event.stopPropagation();
 }
 
 function finishExam(event) {
-	sessionStorage.clear();
-	window.location.reload();
+    if (!confirm("İmtahan sonlandirilsin?")) return;
+
+    let questionsCount = questionsArray.length;
+    let right = 0, wrong = 0, notAnswered = 0;
+    startExamBtn.textContent = "İmtahana yenidən başla";
+    startExamBtn.parentElement.style.display = "flex";
+    quizContainer.style.display = "none";
+    questionNumbers[currentQuestionIndex].classList.remove("selected-question");
+
+    for (let question of questionsArray) {
+        question.selected === null ?
+            notAnswered++ :
+            question.selected === question.ra ?
+            right++ :
+            wrong++;
+    }
+
+    showResult(questionsCount, right, wrong, notAnswered);
+    sessionStorage.clear();
     event.stopPropagation();
 }
 
-function setQuestion(index) {
-	currentQuestionIndex = index > 14 ?
-	    0 :
-	    index < 0 ?
-	    14 :
-	    index;
+function setQuestion(index, previousQuestionIndex) {
+    currentQuestionIndex = index > 14 ? 0 : index < 0 ? 14 : index;
+    sessionStorage.setItem("currentQuestion", currentQuestionIndex);
+    let questionObject = questionsArray[currentQuestionIndex];
+    questionNumbers[previousQuestionIndex].classList.remove("selected-question");
+    questionNumbers[currentQuestionIndex].classList.add("selected-question");
 
-	let questionObject = questionsArray[currentQuestionIndex];
-
-	if (!questionObject) {
+    if (!questionObject) {
         questionContainer.textContent = "";
         variantsList.style.display = "none";
-	    return;
+        return;
     }
 
-	questionContainer.textContent = questionObject.question;
+    questionContainer.textContent = questionObject.question;
     variantsList.style.display = "block";
 
-	for (let i = 0; i < 4; i++) {
-		let li = variantsList.children[i];
+    for (let i = 0; i < 4; i++) {
+        let li = variantsList.children[i];
 
-		i === questionObject.selected ?
+        i === questionObject.selected ?
             li.firstElementChild.checked = true :
             li.firstElementChild.checked = false;
 
-		li.lastElementChild.textContent = questionObject["answers"][i];
-	}
+        li.lastElementChild.textContent = questionObject["answers"][i];
+    }
 }
 
 function setRandomVariants(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
+    let ra = 0;
+
+    for (let i = 3; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+        ra = (i === ra) ? j : (j === ra) ? i : ra;
+    }
+
+    return ra;
 }
 
 function chooseOption(event) {
-	let questionObject = questionsArray[currentQuestionIndex];
-	questionObject.selected = +event.target.value;
-	sessionStorage.setItem("questions", JSON.stringify(questionsArray));
+    let questionObject = questionsArray[currentQuestionIndex];
+    questionObject.selected = +event.target.value;
+    sessionStorage.setItem("questions", JSON.stringify(questionsArray));
+}
+
+function showResult(a, b, c, d) {
+    resultContainer.classList.add("d-flex", "center");
+    resultFields[0].textContent = a;
+    resultFields[1].textContent = b;
+    resultFields[2].textContent = c;
+    resultFields[3].textContent = d;
 }
